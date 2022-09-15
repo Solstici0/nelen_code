5abcfb2d8f4329f5997acc12f9895f2331511b4a
Ismael Jaras <is.jaras@gmail.com>
2022-09-15 18:02:14 -0300

    include logger and dictionaries for odrvs' git push

--- routines/homing.py
+++ routines/homing.py
@@ -1,5 +1,4 @@
 #!/home/rosconcoco/nelen/bin/python
-
 import time
 
 import odrive
@@ -8,16 +7,10 @@ from odrive.utils import *
 
 from config import config
 from utils import exceptions
-odrvc = odrive.find_any(serial_number=config.rc_serial) # codo and z
-odrvh = odrive.find_any(serial_number=config.hombro_serial) # hombro
 
-print('Dumping errors codo-muneca')
-dump_errors(odrvc, True)
-print('Dumping errors hombro')
-dump_errors(odrvh, True)
+from logging.logging import logger
 
-# TODO: Generalize calibration and close_loop_control access
-
+logger = logger.getChild(__name__)
 
 
 def calibrate(axis):
@@ -39,23 +32,29 @@ def calibrate(axis):
 
 
     """
-    #only the encoder must be calibrated, the motor parameters are saved in the odrive
-    #this should check if the axis changed correctly
+    # only the encoder must be calibrated, the motor parameters are saved in 
+    # the odrive
+    # this should check if the axis changed correctly
     axis.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION  
-    exceptions.raise_except(axis,AXIS_STATE_ENCODER_OFFSET_CALIBRATION) #exception if unable to enter requested state
-    exceptions.timeout_except(axis,AXIS_STATE_ENCODER_OFFSET_CALIBRATION) #exception if time out and still in the same state
-    print('succesfully entered AXIS_STATE_ENCODER_OFFSET_CALIBRATION')  
+    # exception if unable to enter requested state
+    exceptions.raise_except(axis,AXIS_STATE_ENCODER_OFFSET_CALIBRATION) 
+    # exception if time out and still in the same state
+    exceptions.timeout_except(axis,AXIS_STATE_ENCODER_OFFSET_CALIBRATION) 
+    logger.info('succesfully entered AXIS_STATE_ENCODER_OFFSET_CALIBRATION')  
     
     #execute homing
     axis.requested_state = AXIS_STATE_HOMING 
-    exceptions.raise_except(axis,AXIS_STATE_HOMING) #exception if unable to enter requested state
-    exceptions.timeout_except(axis,AXIS_STATE_HOMING)  #exception if time out and still in the same state
-    print('Current axis successfully homed')
+    # exception if unable to enter requested state
+    exceptions.raise_except(axis,AXIS_STATE_HOMING) 
+    # exception if time out and still in the same state
+    exceptions.timeout_except(axis,AXIS_STATE_HOMING)  
+    logger.info('Current axis successfully homed')
     
     #execute control loop
     axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    exceptions.raise_except(axis, AXIS_STATE_CLOSED_LOOP_CONTROL) #exception if unable to enter requested state
-    print('Current axis successfully enters control mod3')
+    # exception if unable to enter requested state
+    exceptions.raise_except(axis, AXIS_STATE_CLOSED_LOOP_CONTROL) 
+    logger.info('Current axis successfully enters control mod3')
 
 
 # straight
@@ -73,25 +72,56 @@ def calibrate(axis):
 #     odrvh.axis0.controller.move_incremental(offset_h_stg, False)
 #     time.sleep(5)
 
-
 if __name__=='__init__':
+    import argarse
+    parser = argparse.ArgumentParser(description="Nelen homing",
+                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-j", "--joint", type=str, required=True,
+            help="Joint to calibrate: \ 
+                    hombro, \
+                    codo, \ 
+                    z, \
+                    all")
+    args = parser.parse_args()
+    joint_to_calibrate = args.joint
   
     # odrv radio cubito, axis 0->Z, 1->codo
-    
-    #Z
-    print('Working on z')
-    calibrate(odrvc.axis0)
-    print('z calibrated')
+    odrv_d = {}
+    if joint_to_calibrate == 'all':
+        odrv['codo-muneca'] = odrive.find_any(
+                serial_number=config.rc_serial) # codo and z
+        odrv['hombro'] = odrive.find_any(
+                serial_number=config.hombro_serial) # hombro
+    elif joint_to_calibrate in ['codo', 'z']:
+        odrv['codo-muneca'] = odrive.find_any(
+                serial_number=config.rc_serial) # codo and z
+    elif joint_to_calibrate == 'hombro':
+        odrv['hombro'] = odrive.find_any(
+                serial_number=config.hombro_serial) # hombro
+    else:
+        raise KeyError(f'there is no {joint_to_calibrate} joint')
 
-    ## Hombro
-    print('Working on Hombro')
-    calibrate(odrvh.axis0)
-    print('hombro calibrated')
-
+    for odrv_name in odrv_d:
+        logger.info('Dumping errors for %s odrive: %s',odrv_name,
+                dump_errors(odrvc))
+        dump_errors(odrv[odrv_name], True)
     
-    #codo
-    print('Working on codo')
-    calibrate(odrvc.axis1)
-    print('hombro calibrated')
-    print("fin")
+    for odrv in odrv_d:
+        if odrv == 'codo-muneca':
+            #Z
+            logger.info('Working on z')
+            calibrate(odrvc.axis0)
+            logger.infoprint('z calibrated')
+
+            #codo
+            logger.info('Working on codo')
+            calibrate(odrvc.axis1)
+            logger.info('hombro calibrated')
+        elif odrv = 'hombro':
+            ## Hombro
+            logger.info('Working on Hombro')
+            calibrate(odrvh.axis0)
+            logger.info('hombro calibrated')
+    logger.info("End calibration")
+
 
